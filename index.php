035a0267e39595eab9ac301dc0545c1ef9cbd0d3
Nicolas Rodriguez <rodriguez.supervielle.nicolas@gmail.com>
2023-03-18 07:58:11 -0300

    cioonooss

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
     <header>
       <nav>
         <ul class="encabezado__menu">
-            <li><h3><a href="index.php">PROBANDO</a></h3></li>
-            <li><h3><a href="#miranos">CAMBIARLOS</a> </h3></li>
+            <li><h3><a href="index.php">INICIOOO</a></h3></li>
+            <li><h3><a href="#miranos">MIRANOSSS</a> </h3></li>
             <li><h3><a href="#discografia">MATERIAL</a> </h3></li>   
             <li><h3><a href="#seguinos"> SEGUINOS</a></h3></li>
             <li><h3><a href="novedades.php">LETRAS</a></h3></li>
